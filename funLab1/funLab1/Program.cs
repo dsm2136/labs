866b4f0d90d6b6ff46c0552401b0cb63a80c6d34
dsm2136 <brodsm1487@gmail.com>
2019-03-12 10:22:17 +0300

    1

---
+++ funLab1/funLab1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое слогаемое!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе слогаемое!");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int res = MathOperations.CubSumi(a, b);
+
+            Console.WriteLine("результат: " + res);
+
+            Console.ReadKey();
+        }
+    }
+
+    class MathOperations
+    {
+        public static int CubSumi(int arg1, int arg2)
+        {
+            int sum = arg1 + arg2;
+            return sum * sum * sum;
+        }
+    }
+}
